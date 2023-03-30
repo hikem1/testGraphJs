91c1b611ba746041237a38114a754cd11875d04d
rouchon Fabien <hikem1@hotmail.fr>
2023-03-30 07:12:41 +0200

    first commit

---
+++ appSettings.js
@@ -0,0 +1,10 @@
+export const settings = {
+    'clientId': 'a559fb6d-8a9c-443c-92fd-20f084cae946',
+    'tenantId': '1d5b09e7-4568-44b6-bbe8-f39fdc87bea8',
+    'graphUserScopes': [
+        'user.read',
+        'mail.read',
+        'mail.send'
+    ]
+};
+
